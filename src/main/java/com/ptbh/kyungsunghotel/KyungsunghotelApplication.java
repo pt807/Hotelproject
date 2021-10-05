71ae705284ca43895702d773a458ff93611f1cd9
pt807 <pt807@naver.com>
2021-10-05 20:45:46 +0900

    init

---
+++ src/main/java/com/ptbh/kyungsunghotel/KyungsunghotelApplication.java
@@ -0,0 +1,13 @@
+package com.ptbh.kyungsunghotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyungsunghotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyungsunghotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptbh/kyungsunghotel/KyungsunghotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptbh.kyungsunghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyungsunghotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
