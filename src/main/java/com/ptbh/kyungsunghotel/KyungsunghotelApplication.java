960bd67acbb608f42cc21142cd22c76d83c7e6ae
hunbk <shyun823@naver.com>
2021-10-11 22:03:00 +0900

    CommandLineRunner 사용해서 더미 데이터 추가

--- src/main/java/com/ptbh/kyungsunghotel/KyungsunghotelApplication.java
+++ src/main/java/com/ptbh/kyungsunghotel/KyungsunghotelApplication.java
@@ -1,13 +1,40 @@
 package com.ptbh.kyungsunghotel;
 
+import com.ptbh.kyungsunghotel.board.Board;
+import com.ptbh.kyungsunghotel.board.BoardRepository;
+import com.ptbh.kyungsunghotel.member.Member;
+import com.ptbh.kyungsunghotel.member.MemberRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class KyungsunghotelApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(KyungsunghotelApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(MemberRepository memberRepository, BoardRepository boardRepository) throws Exception {
+		return (args) -> {
+			memberRepository.save(Member.builder().loginId("test").password("test").name("홍길동").email("test@test.com").telephone("01012345678").build());
+			IntStream.rangeClosed(1, 10).forEach(index ->
+					boardRepository.save(Board.builder()
+							.writer("홍길동")
+							.title("게시글 " + index)
+							.content("내용" + index)
+							.createTime(LocalDateTime.now())
+							.build()
+					)
+			);
+		};
+	}
 }
