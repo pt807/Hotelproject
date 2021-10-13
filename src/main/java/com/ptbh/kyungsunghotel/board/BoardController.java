3ce6d685d8de67f51fc3344deea1d7a720633f28
pt807 <pt807@naver.com>
2021-10-14 02:26:18 +0900

    글작성,저장 기능추가, 글작성 버튼 회원 권한 부여

--- src/main/java/com/ptbh/kyungsunghotel/board/BoardController.java
+++ src/main/java/com/ptbh/kyungsunghotel/board/BoardController.java
@@ -2,12 +2,18 @@ package com.ptbh.kyungsunghotel.board;
 
 
 import com.ptbh.kyungsunghotel.member.Member;
+import com.ptbh.kyungsunghotel.member.MemberRepository;
 import com.ptbh.kyungsunghotel.member.SessionConstants;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.constraints.Past;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,9 +30,32 @@ public class BoardController {
         if (loginMember != null) {
             model.addAttribute("member", loginMember);
         }
-
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "boards/list";
     }
+
+    @GetMapping("/board/post")
+    public String showPost(Model model) {
+        model.addAttribute("postForm", new PostForm());
+        return "/boards/post";
+    }
+
+    @PostMapping("/board/post")
+    public String post(@Validated PostForm postForm, BindingResult bindingResult, HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            return "/boards/post";
+        }
+        Board board = new Board();
+       // board.setWriter(session.getId());
+        board.setTitle(postForm.getTitle());
+        board.setContent(postForm.getContent());
+        board.setCreateTime(LocalDateTime.now());
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
+
+
 }
+
+
--- src/main/java/com/ptbh/kyungsunghotel/board/PostForm.java
+++ src/main/java/com/ptbh/kyungsunghotel/board/PostForm.java
@@ -3,10 +3,24 @@ package com.ptbh.kyungsunghotel.board;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Data
 public class PostForm {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer boardNo;
+
+    @Column
+    private String writer;
+
+
     @Column
     @NotBlank(message = "제목은 필수입니다")
     private String title;
@@ -14,6 +28,9 @@ public class PostForm {
     @Column
     @NotBlank(message = "내용은 필수입니다")
     private String content;
+
+    @Column
+    private LocalDateTime createTime;
 
     public PostForm() {
     }
